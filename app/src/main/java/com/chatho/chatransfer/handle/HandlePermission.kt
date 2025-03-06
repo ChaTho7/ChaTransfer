2adc8b62c3c52588c10502d0a62d4797054e0905
ChaTho7 <unique0855@gmail.com>
2025-03-06 17:27:34 +0300

    MANAGE_EXTERNAL_STORAGE permission fixed for later of API 30

--- app/src/main/java/com/chatho/chatransfer/handle/HandlePermission.kt
+++ app/src/main/java/com/chatho/chatransfer/handle/HandlePermission.kt
@@ -2,15 +2,25 @@ package com.chatho.chatransfer.handle
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Environment
+import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class HandlePermission(private val activity: AppCompatActivity) {
+    private val requestManageStorage =
+        activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            requestManageStorageCallback()
+        }
+
     companion object {
         private const val TAG = "MainActivity"
         private const val PERMISSION_REQUESTS = 1
@@ -36,6 +46,12 @@ class HandlePermission(private val activity: AppCompatActivity) {
     }
 
     fun getRuntimePermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+            intent.data = Uri.parse("package:${activity.packageName}")
+            requestManageStorage.launch(intent)
+        }
+
         val permissionsToRequest = ArrayList<String>()
         for (permission in REQUIRED_RUNTIME_PERMISSIONS) {
             permission.let {
@@ -49,6 +65,16 @@ class HandlePermission(private val activity: AppCompatActivity) {
             ActivityCompat.requestPermissions(
                 activity, permissionsToRequest.toTypedArray(), PERMISSION_REQUESTS
             )
+        }
+    }
+
+    private fun requestManageStorageCallback() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+            Toast.makeText(activity, "You have to give access to all files.", Toast.LENGTH_SHORT)
+                .show()
+            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+            intent.data = Uri.parse("package:${activity.packageName}")
+            requestManageStorage.launch(intent)
         }
     }
 
